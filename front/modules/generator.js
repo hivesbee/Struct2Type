8c4e1e7f45580fdbea097f6d693422ec685b8269
hivesbee <hivesbee@gmail.com>
2020-11-01 22:46:26 +0900

    migrate to next.js

---
+++ front/modules/generator.js
@@ -0,0 +1,30 @@
+const typeSwitchMap = {
+  'bool': 'boolean',
+  'int': 'number',
+  '*time.Time': 'string'
+}
+
+const switchTypeName = (type) => typeSwitchMap[type] || type
+
+const generate = (json) => {
+  // type 定義
+  const head = `type ${json.name} = {`
+
+  // field 定義
+  let fields = []
+  if (json.fields) {
+    fields = json.fields
+      .filter(f => f.name && f.type && f.tag)
+      .map(f => {
+        const name = f.tag.replace(/'|"|`/g, '').split(':')[1]
+
+        return `  ${name}: ${switchTypeName(f.type)}`
+      })
+  }
+
+  return [head, ...fields, '}'].join('\n')
+}
+
+export {
+  generate
+}
---
+++ front/modules/theme.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from 'styled-components'
+
+const baseTheme: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    brand: '#66CaF8',
+    base: '#FCFCFC',
+    danger: '#EE6352',
+    success: '#6CB284',
+    important: '#00487C',
+    border: '#DDDDDD'
+  },
+}
+
+export { baseTheme }
